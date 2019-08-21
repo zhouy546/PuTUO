54cdb800169a0ccfcb6286b69b71d84e197f140c
YunqiZhouJoe <zhouyunqi546@gmail.com>
2019-08-21 19:24:13 +0800

    Initial commit

---
+++ Assets/Scripts/LogoJobSystem.cs
@@ -0,0 +1,30 @@
+﻿using CodeMonkey.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogoJobSystem : MonoBehaviour
+{
+    [SerializeField]Transform testTransform;
+    public Vector3 MousePos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            Debug.Log(UtilsClass.GetMouseWorldPosition());
+             Vector3 pos = UtilsClass.GetMouseWorldPosition();
+
+            testTransform.position = new Vector3(pos.x, pos.y, 10);
+
+
+
+        }
+       
+    }
+}
---
+++ Assets/Scripts/mLogo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum FlapState {
+Front,Back
+}
+
+public class mLogo : MonoBehaviour
+{
+    public Transform m_transform;
+    public float FlapSpeed =1f;
+    public FlapState flapState = FlapState.Back;
+
+    void Awake() {
+        m_transform = this.GetComponent<Transform>();
+    }
+    
+}
